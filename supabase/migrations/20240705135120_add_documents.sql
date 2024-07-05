01444189478811e7dfbdd62b2dd74681f623d4d1
Alex Kates <alex.kates@croissant.com>
2024-07-05 10:21:12 -0400

    add migrations

---
+++ supabase/migrations/20240705135120_add_documents.sql
@@ -0,0 +1,47 @@
+-- Supabase AI is experimental and may produce incorrect answers
+-- Always verify the output before executing
+-- Drop the function if it exists
+DROP FUNCTION IF EXISTS match_documents (vector (1536), INT, jsonb);
+
+-- Drop the table if it exists
+DROP TABLE IF EXISTS documents;
+
+-- Enable the pgvector extension to work with embedding vectors
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Create a table to store your documents
+create table
+  documents (
+    id bigserial primary key,
+    content text, -- corresponds to Document.pageContent
+    metadata jsonb, -- corresponds to Document.metadata
+    embedding vector (1536) -- 1536 works for OpenAI embeddings, change if needed
+  );
+
+-- Create a function to search for documents
+create function match_documents (
+  query_embedding vector (1536),
+  match_count int DEFAULT null,
+  filter jsonb DEFAULT '{}'
+) returns table (
+  id bigint,
+  content text,
+  metadata jsonb,
+  embedding jsonb,
+  similarity float
+) language plpgsql as $$
+#variable_conflict use_column
+begin
+  return query
+  select
+    id,
+    content,
+    metadata,
+    (embedding::text)::jsonb as embedding,
+    1 - (documents.embedding <=> query_embedding) as similarity
+  from documents
+  where metadata @> filter
+  order by documents.embedding <=> query_embedding
+  limit match_count;
+end;
+$$;
