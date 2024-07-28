86a9577f2082ca89a6244d0873b6161a013440ac
Alex Kates <alex.kates@croissant.com>
2024-07-28 16:53:43 -0400

    refactor: Remove unused 'sources' parameter from ChatMessage component

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -5,18 +5,19 @@ import remarkMath from "remark-math";
 
 type Props = {
   message: Message;
-  sources?: any[];
 };
 
-export function ChatMessage({ message, sources }: Props) {
-  const distinctSources = Array.from(new Set(sources?.map((source) => source.metadata.title)));
-
+export function ChatMessage({ message }: Props) {
   const alignmentClassName = message.role === "user" ? "ml-auto" : "mr-auto";
   const colorClassNames =
     message.role === "user" ? "bg-primary text-primary-foreground" : "bg-secondary text-secondary-foreground";
 
+  const borderRoundedClassNames = message.role === "user" ? "rounded-br-none" : "rounded-bl-none";
+
   return (
-    <div className={`${alignmentClassName} ${colorClassNames} mb-8 flex max-w-[80%] flex-col gap-4 rounded px-4 py-2`}>
+    <div
+      className={`${alignmentClassName} ${colorClassNames} ${borderRoundedClassNames} relative mb-8 flex max-w-[80%] flex-col gap-4 rounded-xl px-4 py-2`}
+    >
       <MemoizedReactMarkdown
         className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 break-words"
         remarkPlugins={[remarkGfm, remarkMath]}
@@ -28,25 +29,6 @@ export function ChatMessage({ message, sources }: Props) {
       >
         {message.content}
       </MemoizedReactMarkdown>
-      {distinctSources?.length > 0 && (
-        <ul>
-          {distinctSources.map((title, i) => {
-            const source = sources?.find((source) => source.metadata.title === title);
-            return (
-              <li key={i}>
-                <a
-                  href={source.metadata.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-xs text-muted-foreground underline"
-                >
-                  {source.metadata.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      )}
     </div>
   );
 }
